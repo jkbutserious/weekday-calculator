a5378c648fd1cfb40de46265591450a8a9340c4f
Jeff Kim <kim2jy@hotmail.com>
2020-06-25 09:42:00 -0700

    Add second failing test
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- __tests__/weekday-calcuater.tests.js
+++ __tests__/weekday-calcuater.tests.js
@@ -2,9 +2,11 @@ import { Weekday } from './../src/weekday-calculator.js';
 
 describe('Weekday', () => {
   let reusableDate;
+  let reusableDate2;
 
   beforeEach(() => {
     reusableDate = new Date(70, 0, 1);
+    reusableDate2 = new Date(70, 0, 2)
   });
 
   test('Should correctly create a date that can return a year, month, and date', () => {
@@ -13,4 +15,7 @@ describe('Weekday', () => {
     expect(reusableDate.getDate()).toEqual(1);
   });
 
+  test('Should correctly return the day of the week based on the date passed into the date object', () => {
+    expect(reusableDate2.getDay()).toEqual(0);
+  })
 })
