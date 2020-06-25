7a9bc15a9c5cd9f31a5b2ca238429781d50a2f64
Jeff Kim <kim2jy@hotmail.com>
2020-06-25 15:37:22 -0700

    Attempt to add a custom algorithm to calculate the day of the week based on a given year by a user
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- src/weekday-calculator.js
+++ src/weekday-calculator.js
@@ -39,3 +39,28 @@ export function checkValidDate(year, month, day) {
     return true;
   }
 }
+
+
+
+
+const leapYears = (givenYear) => {
+  let isLeapYear = 0;
+  for (i = 0; i <= (givenYear % 400); i++) {
+    if (i % 4 === 0 && i % 100 != 0 || i % 400 === 0) {
+      isLeapYear += 1;
+    };
+  }
+  return isLeapYear;
+}
+
+// # of days advanced from given weekday
+let daysAdvanced = (givenYear % 400) % 7
+// # of days "skipped" by leap years
+let daysLeaped = (leapYears % 7)
+
+(givenDay + daysAdvanced + daysLeaped) % 7 
+
+// Given Year is 1, 1, 2000
+
+
+w = ((day + (2.6 * month - 0.2) + year + (year / 4) + (century / 4 - (2 * century)) % 7) if negative add 7
