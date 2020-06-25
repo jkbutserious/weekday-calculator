fa7d8fe5f308ed2aec01a034f5d93c3f87ef53a8
Jeff Kim <kim2jy@hotmail.com>
2020-06-25 09:41:09 -0700

    Add first successful test
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- __tests__/weekday-calcuater.tests.js
+++ __tests__/weekday-calcuater.tests.js
@@ -4,12 +4,13 @@ describe('Weekday', () => {
   let reusableDate;
 
   beforeEach(() => {
-    reusableDate = new Date(0, 1, 2)
+    reusableDate = new Date(70, 0, 1);
   });
 
-  test('Should correctly create a date object with a month, a day, and a year', () => {
+  test('Should correctly create a date that can return a year, month, and date', () => {
     expect(reusableDate.getFullYear()).toEqual(1970);
     expect(reusableDate.getMonth()).toEqual(0);
     expect(reusableDate.getDate()).toEqual(1);
   });
+
 })
